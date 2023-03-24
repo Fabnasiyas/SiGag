1ffe69b08c0867c56b373fe6d1790ad1bb061f9d
Fabna <fabnasiyasck@gmail.com>
2023-03-24 14:02:46 +0530

    server error

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const userRouter = require("./routes/user")
 const adminRouter=require("./routes/admin")
 const path=require('path')
 var session=require('express-session')
-// const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo');
 
 // const morgan= require('morgan')
 const hbs=require('hbs')
@@ -24,7 +24,7 @@ app.use(session({
   cookie:{maxAge:600000},
   resave:true,
   saveUninitialized:false,
-  // store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/project-sigag' })
+  store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/project-sigag' })
 }))
 
 app.set('views', path.join(__dirname, 'views'));
--- config/dbConnect.js
+++ config/dbConnect.js
@@ -5,7 +5,7 @@ const dbConnect=()=>{
     mongoose.set('strictQuery', true);
     mongoose.connect('mongodb://127.0.0.1:27017/project-sigag')
     .then(() => console.log('Connected!')).catch(err=>{
-        console.log("error : ", err)
+        console.log("error sdf: ", err)
     })
 }
 
